fbd1837cb43c312823228e94248080f9e54a98f5
ryan <ryan.blackledge@shibumi.com>
2022-11-30 22:26:34 -0500

    Remove placeholder site and add dark mode selector

---
+++ src/Components/DarkModeSelector.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+import { useColorScheme } from '@mui/joy/styles/CssVarsProvider';
+import IconButton from "@mui/joy/IconButton";
+
+export const DarkModeSelector = () => {
+    const { mode, setMode } = useColorScheme();
+
+    return (
+        <IconButton
+            variant="outlined"
+            onClick={() => {
+                if (mode === 'light') {
+                    setMode('dark');
+                } else {
+                    setMode('light');
+                }
+            }}
+        >
+            {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+        </IconButton>
+    );
+}
